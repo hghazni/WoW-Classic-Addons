62d642d34a27f3df3b6cd5fcf069e7ea49cf3cab
Harry Ghazni <hghazni@gmail.com>
2019-08-25 17:19:16 +0100

    adds many new addons

---
+++ AtlasLootClassic-v1.1.5-beta-classic/AtlasLootClassic_Crafting/Locales/constants.de.lua
@@ -0,0 +1,13 @@
+local AL = _G.AtlasLoot.GetLocales("deDE")
+
+if not AL then return end
+
+-- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
+-- Crafting
+AL["Axes"] = "Äxte"
+AL["Enhancements"] = "Verbesserungen"
+AL["Maces"] = "Kolben"
+AL["Oil"] = "Öl"
+AL["Pets"] = "Haustiere"
+AL["Swords"] = "Schwerter"
+
